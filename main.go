168ed38ffdeb310124ad862e907e14de0b769a4e
Benjamin Kvinen Røiland <benjamin.roiland@hotmail.no>
2026-02-18 21:04:28 +0100

    made struct to hold backends, a way to track backends and initialize server

--- main.go
+++ main.go
@@ -2,3 +2,23 @@ package main
 
 import("fmt")
 
+//struct to hold our backends
+type Backend struct {
+	URL *url.URL
+	Alive bool
+	mux sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+//we need a way to track all backends
+type ServerPool struct {
+	backends []*Backend
+	current uint64
+}
+
+u, _ := url.Parse("http://localhost:8080")
+rb := httputil.NewSingleHostReverseProxy(u)
+
+//initialize server and add this as handler
+http.HandlerFunc(rp.ServeHTTP)
+
